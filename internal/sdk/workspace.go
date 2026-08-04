dde163b19dd0c21468070838c7a7f0d05424a4a5
Voiceflow <serviceaccount@voiceflow.com>
2026-08-04 18:04:04 +0000

    feat: autogenerate commands from OpenAPI spec

--- internal/sdk/workspace.go
+++ internal/sdk/workspace.go
@@ -328,8 +328,8 @@ func (s *Workspace) Create(ctx context.Context, request components.StableWorkspa
 
 }
 
-// Get project
-// Get a project by ID.
+// Get workspace
+// Get a single workspace by ID.
 func (s *Workspace) Get(ctx context.Context, request operations.StableWorkspaceControllerGetRequest, opts ...operations.Option) (*operations.StableWorkspaceControllerGetResponse, error) {
 	o := operations.Options{}
 	supportedOptions := []string{
