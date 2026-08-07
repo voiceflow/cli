691dc9649ad7f0685840c6abecc340242c2ed573
Voiceflow <serviceaccount@voiceflow.com>
2026-08-07 14:37:53 +0000

    feat: autogenerate commands from OpenAPI spec

--- internal/cli/version.go
+++ internal/cli/version.go
@@ -14,7 +14,7 @@ import (
 // which propagates the value here (see cmd/vf/main.go):
 //
 //	go build -ldflags "-X main.version=x.y.z" ./cmd/vf
-var Version = "0.2.90"
+var Version = "0.2.91"
 
 // BuildTime is optionally set at build time via ldflags targeting the main package.
 var BuildTime string
