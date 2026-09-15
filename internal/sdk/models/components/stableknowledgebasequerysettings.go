5a0e429a5930bb578e3ee3bcdfee2a6311ddeac9
Voiceflow <serviceaccount@voiceflow.com>
2026-09-15 23:56:41 +0000

    feat: autogenerate commands from OpenAPI spec

--- internal/sdk/models/components/stableknowledgebasequerysettings.go
+++ internal/sdk/models/components/stableknowledgebasequerysettings.go
@@ -52,6 +52,7 @@ const (
 	StableKnowledgeBaseQuerySettingsModelBedrockClaude48Opus       StableKnowledgeBaseQuerySettingsModel = "bedrock-claude-4.8-opus"
 	StableKnowledgeBaseQuerySettingsModelVoiceflowCore40           StableKnowledgeBaseQuerySettingsModel = "voiceflow-core-4.0"
 	StableKnowledgeBaseQuerySettingsModelVoiceflowCore41           StableKnowledgeBaseQuerySettingsModel = "voiceflow-core-4.1"
+	StableKnowledgeBaseQuerySettingsModelVoiceflowCore42           StableKnowledgeBaseQuerySettingsModel = "voiceflow-core-4.2"
 	StableKnowledgeBaseQuerySettingsModelVoiceflowFlash42          StableKnowledgeBaseQuerySettingsModel = "voiceflow-flash-4.2"
 	StableKnowledgeBaseQuerySettingsModelKimiK3                    StableKnowledgeBaseQuerySettingsModel = "kimi-k3"
 	StableKnowledgeBaseQuerySettingsModelGemini25Pro               StableKnowledgeBaseQuerySettingsModel = "gemini-2.5-pro"
@@ -173,6 +174,8 @@ func (e *StableKnowledgeBaseQuerySettingsModel) UnmarshalJSON(data []byte) error
 	case "voiceflow-core-4.0":
 		fallthrough
 	case "voiceflow-core-4.1":
+		fallthrough
+	case "voiceflow-core-4.2":
 		fallthrough
 	case "voiceflow-flash-4.2":
 		fallthrough
